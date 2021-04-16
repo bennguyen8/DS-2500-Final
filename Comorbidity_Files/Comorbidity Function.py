546dba0d0f3536b0fdd4c269a97fbc95c06ee373
bennguyen8 <50051581+bennguyen8@users.noreply.github.com>
2021-04-16 18:23:39 -0400

    Update Comorbidity Function.py

--- Comorbidity_Files/Comorbidity Function.py
+++ Comorbidity_Files/Comorbidity Function.py
@@ -1,3 +1,6 @@
+import pandas as pd
+import random
+
 def comorbidities():
     '''Gives score for comorbidity section for vaccine distribution'''
     df = pd.read_csv('Conditions_contributing_to_deaths_involving_coronavirus_disease_2019__COVID-19___by_age_group_and_state__United_States..csv')
@@ -36,4 +39,12 @@ def comorbidities():
     elif choice.lower() == 'n':
         score = 0
         return score
-        
+    
+random_list = []
+# identical to condition_values but with 0 to show having no comorbidity on list
+scores = [100, 20, 25, 80, 5, 10, 40, 25, 30, 20, 20, 15, 17.5, 10, 12.5, 25, 10, 35, 10, 10, 25, 20, 0]
+for i in range(1, 1001):
+    random_num = random.choice(scores)
+    random_list.append(random_num)
+    
+comorbidity_df = pd.DataFrame(random_list,columns=['Comorbidities'])
