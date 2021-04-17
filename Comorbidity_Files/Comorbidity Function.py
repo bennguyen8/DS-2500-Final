89539b88dc61301957dcc782f27fd688169d75bd
bennguyen8 <50051581+bennguyen8@users.noreply.github.com>
2021-04-17 13:51:07 -0400

    Update Comorbidity Function.py

--- Comorbidity_Files/Comorbidity Function.py
+++ Comorbidity_Files/Comorbidity Function.py
@@ -25,7 +25,7 @@ def comorbidities():
     conditions.remove('COVID-19') # value representing everyone who contracted COVID-19
     
     # value associated with each comorbidity
-    condition_values = [100, 20, 25, 80, 5, 10, 40, 25, 30, 20, 20, 15, 17.5, 10, 12.5, 25, 10, 35, 10, 10, 25, 20] 
+    condition_values = [100, 20, 25, 80, 5, 10, 40, 25, 30, 20, 20, 15, 17.5, 10, 12.5, 25, 10, 35, 10, 10, 25, 80] 
     # dictionary with comorbidites and values
     condition_dict = dict(zip(conditions, condition_values))
     
@@ -38,15 +38,4 @@ def comorbidities():
         return score
     elif choice.lower() == 'n':
         score = 0
-        return score
-    
-def generate_df():
-    random_list = []
-    # identical to condition_values but with 0 to show having no comorbidity on list
-    scores = [100, 20, 25, 80, 5, 10, 40, 25, 30, 20, 20, 15, 17.5, 10, 12.5, 25, 10, 35, 10, 10, 25, 20, 0]
-    for i in range(1, 1001):
-        random_num = random.choice(scores)
-        random_list.append(random_num)
-
-    comorbidity_df = pd.DataFrame(random_list,columns=['Comorbidities'])
-    return comorbidity_df
+        return score   
